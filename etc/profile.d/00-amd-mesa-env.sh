bcf2d64f44fb552180dea9d481202b307d47d138
Ganesh Belgur Ramachandra <ganesh.belgurramachandra@amd.com>
2023-08-04 05:34:08 -0500

    Shell profile environment file for /opt path installations

---
+++ etc/profile.d/00-amd-mesa-env.sh
@@ -0,0 +1,4 @@
+export PATH="$PATH:/opt/gbelgurr-amd/llvm-project/llvm/bin:/opt/gbelgurr-amd/waffle/bin"
+export LIBGL_DRIVERS_PATH="/opt/gbelgurr-amd/mesa/lib/x86_64-linux-gnu/dri" # needed again even though present in /etc/environment
+export LIBVA_DRIVERS_PATH="/opt/gbelgurr-amd/mesa/lib/x86_64-linux-gnu/dri"
+export PKG_CONFIG_PATH="/opt/gbelgurr-amd/drm/lib/x86_64-linux-gnu/pkgconfig:/opt/gbelgurr-amd/mesa/lib/x86_64-linux-gnu/pkgconfig:/opt/gbelgurr-amd/waffle/lib/pkgconfig:$PKG_CONFIG_PATH"
